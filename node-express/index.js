c080f41979c36f788675b0edb1e2665462696b1d
shan30071997 <shankargawali9665589058@gmail.com>
2020-03-27 15:05:04 +0530

    Express Generator

--- node-express/index.js
+++ node-express/index.js
@@ -1,24 +1,30 @@
-const express = require('express'),
-     http = require('http');
-const morgan = require('morgan');
+const express = require("express");
+http = require("http");
+const morgan = require("morgan");
 
+const bodyParser = require("body-parser");
 
-const hostname = 'localhost';
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
+const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
-
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-
 
 const server = http.createServer(app);
 
